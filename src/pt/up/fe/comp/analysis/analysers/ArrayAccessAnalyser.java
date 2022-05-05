4e710715fb55d40ee40ae458ea92bba6be394a9d
Francisco Colino <francisco.m.colino@gmail.com>
2022-05-05 19:30:09 +0100

    array access with array access index analysis

--- src/pt/up/fe/comp/analysis/analysers/ArrayAccessAnalyser.java
+++ src/pt/up/fe/comp/analysis/analysers/ArrayAccessAnalyser.java
@@ -14,34 +14,40 @@ import java.util.Optional;
 
 public class ArrayAccessAnalyser extends PreorderSemanticAnalyser {
 
-    public ArrayAccessAnalyser(){
+    public ArrayAccessAnalyser() {
         super();
         addVisit(AstNode.ARRAY_ACCESS, this::visitArrayAccess);
     }
 
-    public Integer visitArrayAccess(JmmNode array_access, SymbolTableImpl symbolTable){
+    public Integer visitArrayAccess(JmmNode array_access, SymbolTableImpl symbolTable) {
 
-        Optional<JmmNode> ancestor_id = Optional.ofNullable(array_access.getJmmChild(0));
+        JmmNode ancestor_id = array_access.getJmmChild(0);
 
-        if (ancestor_id.isPresent()){
-            Type ancestor_type =  this.getIdType(ancestor_id.get(), symbolTable);
-            if (!ancestor_type.isArray()){
-                addReport(new Report(
-                        ReportType.ERROR, Stage.SEMANTIC,
-                        Integer.parseInt(array_access.get("line")),
-                        Integer.parseInt(array_access.get("col")),
-                        "Array access only allowed on arrays."));
-            } else if (!Objects.equals(ancestor_type.getName(), "int")){
-                addReport(new Report(
-                        ReportType.ERROR, Stage.SEMANTIC,
-                        Integer.parseInt(array_access.get("line")),
-                        Integer.parseInt(array_access.get("col")),
-                        "Array must be of time int"));
-            }
-        } else {
-            // TODO throw error?
-            System.out.println("ERROR ----------");
-            return -1;
+        JmmNode access_node = array_access.getJmmChild(1);
+
+        Type ancestor_type = this.getIdType(ancestor_id, symbolTable);
+        if (!ancestor_type.isArray()) {
+            addReport(new Report(
+                    ReportType.ERROR, Stage.SEMANTIC,
+                    Integer.parseInt(array_access.get("line")),
+                    Integer.parseInt(array_access.get("col")),
+                    "Array access only allowed on arrays."));
+        }
+        if (!Objects.equals(ancestor_type.getName(), "int")) {
+            addReport(new Report(
+                    ReportType.ERROR, Stage.SEMANTIC,
+                    Integer.parseInt(array_access.get("line")),
+                    Integer.parseInt(array_access.get("col")),
+                    "Array must be of time int"));
+        }
+        // TODO getJmmNodeType should be able to handle IntLiteral by itself
+        if (!(Objects.equals(access_node.getKind(), "IntLiteral") ||
+                Objects.equals(this.getJmmNodeType(access_node, symbolTable).getName(), "integer"))){
+            addReport(new Report(
+                    ReportType.ERROR, Stage.SEMANTIC,
+                    Integer.parseInt(array_access.get("line")),
+                    Integer.parseInt(array_access.get("col")),
+                    "Array access index must be of integer type."));
         }
 
         return 0;
