cc542c117cfa30e4781098c878f1271743017073
Catarina Pires <catarina.o.pires@hotmail.com>
2022-05-13 17:06:46 +0100

    Added tests

--- test/pt/up/fe/comp/OptimizationTest.java
+++ test/pt/up/fe/comp/OptimizationTest.java
@@ -1,7 +1,10 @@
 package pt.up.fe.comp;
 
 import org.junit.Test;
+import pt.up.fe.comp.jmm.jasmin.JasminResult;
 import pt.up.fe.specs.util.SpecsIo;
+
+import static org.junit.Assert.assertEquals;
 
 public class OptimizationTest {
     @Test
@@ -25,6 +28,12 @@ public class OptimizationTest {
     @Test
     public void testLife() {
         var result = TestUtils.optimize(SpecsIo.getResource("fixtures/public/Life.jmm"));
+        TestUtils.noErrors(result.getReports());
+    }
+
+    @Test
+    public void testSelfMade5() {
+        var result = TestUtils.optimize(SpecsIo.getResource("fixtures/public/selfMade/SelfMade5.jmm"));
         TestUtils.noErrors(result.getReports());
     }
 
