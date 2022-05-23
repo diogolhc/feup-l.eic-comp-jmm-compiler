812ff7394b39a79cc9eef2cea27ed4af818c9c19
João Bispo <joaobispo@gmail.com>
2022-05-23 14:56:10 +0100

    Fixes ollirTest_2_02_CompileArithmetic to search for the bin op inside assign instructions
    - Patch by prof. Tiago Carvalho;

--- test/pt/up/fe/comp/Cp2Test.java
+++ test/pt/up/fe/comp/Cp2Test.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2022 SPeCS.
- * <p>
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * <p>
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -320,8 +320,11 @@ public class Cp2Test {
         assertNotNull("Could not find method " + methodName, methodFoo);
 
         var binOpInst = methodFoo.getInstructions().stream()
-                .filter(inst -> inst instanceof BinaryOpInstruction)
+                .filter(inst -> inst instanceof AssignInstruction)
+                .map(instr -> (AssignInstruction)instr)
+                .filter(assign -> assign.getRhs() instanceof BinaryOpInstruction)
                 .findFirst();
+
         assertTrue("Could not find a binary op instruction in method " + methodName, binOpInst.isPresent());
 
         var retInst = methodFoo.getInstructions().stream()
