282ed9c719947a9f79230f0376b615361d0b4bc7
Diogo LuAs Henriques Costa <up201906731@fe.up.pt>
2022-06-11 14:57:17 +0100

    Add test with multiple if styles

--- test/pt/up/fe/comp/BackEndTest.java
+++ test/pt/up/fe/comp/BackEndTest.java
@@ -97,4 +97,12 @@ public class BackEndTest {
         TestUtils.noErrors(jasminResult);
     }
 
+    @Test
+    public void testIfs() {
+        JasminResult jasminResult = TestUtils.backend(SpecsIo.getResource("fixtures/public/selfMade/Ifs.jmm"));
+        jasminResult.compile();
+        assertEquals("10050100\r\n", jasminResult.run());
+        TestUtils.noErrors(jasminResult);
+    }
+
 }
